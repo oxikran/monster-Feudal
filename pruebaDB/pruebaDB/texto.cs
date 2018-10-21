3a627b18a2ee6290459be73846721ec1eb2bc789
oxikran <oxikran@gmail.com>
2018-10-21 10:19:31 +0100

    21/10/18 interfaz beta

---
+++ pruebaDB/pruebaDB/texto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pruebaDB
+{
+    public partial class texto : Form
+    {
+        public texto()
+        {
+            InitializeComponent();
+        }
+
+        private void texto_Load(object sender, EventArgs e,String option)
+        {
+
+            Select Case option{
+
+            }
+
+        }
+    }
+}
